76ecd194a83984b2f48df5c189028e9e66f0057d
tuanpham91 <phamtuanrk1991@gmail.com>
2020-12-22 23:22:36 +0100

    Update

--- file-server.js
+++ file-server.js
@@ -1,13 +1,21 @@
 var express = require("express");
-
+var path = require("path")
 var app = express();
 var config = require("./config.json")
+var fs = require("fs")
 
 // Maybe in the future, you would want to have a path for each user.
-function buildPath(path) {
-    return path;
+function buildPath(relPath) {
+    return path.join(config.env.localPath, relPath) ;
 }
 
+function getFilesFromLocalPath(path, res, callback) {
+    fs.readdir(path, (err, files) => {
+        callback(res, files)
+    })
+}
+
+function handleFile
 // Maybe give us folder here
 app.get(config.api.getFolder, function(req, res) {
     var path = req.query.path;
