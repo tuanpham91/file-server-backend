1fd91a78882b34a16e01abf1da80d073e1b71679
Anh Tuan Pham <anh.pham@jambit.com>
2020-12-23 16:15:39 +0100

    Update

--- file-server.js
+++ file-server.js
@@ -4,22 +4,30 @@ var app = express();
 var config = require("./config.json")
 var fs = require("fs")
 
+var localPath = config.env[process.platform].localPath
 // Maybe in the future, you would want to have a path for each user.
 function buildPath(relPath) {
-    return path.join(config.env.localPath, relPath) ;
+    return path.join(localPath, relPath) ;
 }
 
-function getFilesFromLocalPath(path, res, callback) {
-    fs.readdir(path, (err, files) => {
-        callback(res, files)
+function getFilesFromLocalPath(path) {
+    fs.readdir(path, (_, files) => {
+        console.log(files)
+        return files;
     })
 }
 
-function handleFile
+function handleFileListResult(res, files) {
+    // make json out of this.
+    res.end();
+}
+
 // Maybe give us folder here
 app.get(config.api.getFolder, function(req, res) {
     var path = req.query.path;
-    var localPath = buildPath(path);
-    
-
+    var absPath = buildPath(path);
+    getFilesFromLocalPath(absPath, res, handleFileListResult);
 });
+
+// Test : 
+getFilesFromLocalPath(buildPath(""))
