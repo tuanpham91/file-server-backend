15a6d58c0eea7caf99365fd07e22bcb7ead0aa82
Anh Tuan Pham <anh.pham@jambit.com>
2020-12-24 11:56:35 +0100

    Update

--- file-server.js
+++ file-server.js
@@ -4,30 +4,34 @@ var app = express();
 var config = require("./config.json")
 var fs = require("fs")
 
-var localPath = config.env[process.platform].localPath
+var localPath = config.env[process.platform].localPath;
+var portNumber = 8080;
+
 // Maybe in the future, you would want to have a path for each user.
 function buildPath(relPath) {
     return path.join(localPath, relPath) ;
 }
 
-function getFilesFromLocalPath(path) {
+function getFilesFromLocalPath(path, res, callback) {
     fs.readdir(path, (_, files) => {
-        console.log(files)
-        return files;
+        callback(res, files);
     })
 }
 
 function handleFileListResult(res, files) {
     // make json out of this.
-    res.end();
+    var result = JSON.stringify(files);
+    console.log(files);
+    res.send(result);
 }
 
 // Maybe give us folder here
-app.get(config.api.getFolder, function(req, res) {
-    var path = req.query.path;
+app.get("/", function(req, res) {
+    var path = typeof(req.query.path) === "undefined" ? "" : req.query.path;
     var absPath = buildPath(path);
-    getFilesFromLocalPath(absPath, res, handleFileListResult);
+    getFilesFromLocalPath(absPath, res, handleFileListResult)
 });
 
-// Test : 
-getFilesFromLocalPath(buildPath(""))
+app.listen(portNumber, () => {
+    console.log(`Example app listening at http://localhost:8080`)
+})
